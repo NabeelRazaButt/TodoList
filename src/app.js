d4e65c05bab191b70354bcd277bbbeccd94d477c
NabeelRazaButt <buttnabeel6@gmail.com>
2021-01-14 00:09:16 +0500

    Completed

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 App = {
+    loading: false, 
+    contracts:{},
     load: async() =>{
         //load app..
       console.log("App loading...")
       await App.loadWeb3()
       await App.loadAccount()
       await App.loadContract()
+      await App.render()
     },
 
       // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
@@ -23,7 +26,7 @@ App = {
         await ethereum.enable()
         // Acccounts now exposed
         web3.eth.sendTransaction({/* ... */})
-      } catch (error) {
+      } catch (error) { 
         // User denied account access...
       }
     }
@@ -46,15 +49,94 @@ App = {
   },
 
   loadContract: async () => {
-    // var contract = required("truffle-contract")
+    // Create a JavaScript version of the smart contract
     const todoList = await $.getJSON('TodoList.json')
     App.contracts.TodoList = TruffleContract(todoList)
     App.contracts.TodoList.setProvider(App.web3Provider)
+    App.todoList = await App.contracts.TodoList.deployed()
+  
   },
+  render: async () => {
+    //Prevent double render
+    if (App.loading){
+      return
+    }
+    //Update app loading state
+    App.setLoading(true)
+
+    //Render Account
+    $('#account').html(App.account)
+
+    //Render Tasks
+    await App.renderTasks()
+
+    //Update app loading state
+    App.setLoading(false)
+  },
+
+  renderTasks: async () => {
+    // Load the total task count from the blockchain
+    const taskCount = await App.todoList.taskCount()
+    const $taskTemplate = $('.taskTemplate')
+
+    // Render out each task with a new task template
+    for (var i = 1; i <= taskCount; i++) {
+      // Fetch the task data from the blockchain
+      const task = await App.todoList.tasks(i)
+      const taskId = task[0].toNumber()
+      const taskContent = task[1]
+      const taskCompleted = task[2]
+
+      // Create the html for the task
+      const $newTaskTemplate = $taskTemplate.clone()
+      $newTaskTemplate.find('.content').html(taskContent)
+      $newTaskTemplate.find('input')
+                      .prop('name', taskId)
+                      .prop('checked', taskCompleted)
+                      .on('click', App.toggleCompleted)
+
+      // Put the task in the correct list
+      if (taskCompleted) {
+        $('#completedTaskList').append($newTaskTemplate)
+      } else {
+        $('#taskList').append($newTaskTemplate)
+      }
+
+      // Show the task
+      $newTaskTemplate.show()
+    }
+  },
+
+  createTask: async () => {
+    App.setLoading(true)
+    const content = $('#newTask').val()
+    await App.todoList.createTask(content)
+    window.location.reload()
+  },
+  toggleCompleted: async (e) => {
+    App.setLoading(true)
+    const taskId = e.target.name
+    await App.todoList.toggleCompleted(taskId)
+    window.location.reload()
+  },
+
+
+  setLoading: (boolean) => {
+    App.loading = boolean
+    const loader = $('#loader')
+    const content = $('#content')
+    if(boolean) {
+      loader.show()
+      content.hide()
+    } else {
+      loader.hide()
+      content.show()
+    }
+  }
 }
 
     $(() => {
         $(window).load(() => {
             App.load()
         })
-    })
+    }) 
